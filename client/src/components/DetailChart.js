32753a7610cf2cc7ec7a4e89ff878b4caa0d0f4f
Moomi98 <kymkjh2002@gmail.com>
2021-08-13 21:08:45 +0900

    추가수정 : 차트 구현 완료, 내부 내용 사항 수정중

--- client/src/components/DetailChart.js
+++ client/src/components/DetailChart.js
@@ -1,51 +1,64 @@
 import { Line } from 'react-chartjs-2';
 
-const data = {
-  labels: ['1', '2', '3', '4', '5', '6'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgba(255, 99, 132, 0.2)',
-    },
-  ],
-};
+const today = new Date();   
 
-const options = {
-  legend: {
-    display:false
-  },
+const year = today.getFullYear(); // 년도
+const month = today.getMonth() + 1;  // 월
 
-  scales: {
-    yAxes: [
+const recent6Month = (month) => {
+  const monthList = [];
+
+  for(let i = 0 ; i < 6; i++){
+    monthList.push(month + "월");
+    month -= 1;
+    if (month === 0){
+      month = (year - 1) + "-" + 12;
+    }
+  }
+  monthList.reverse();
+  return monthList;
+
+}
+
+const DetailChart = ({recentMonthData}) => {
+  const monthList = recent6Month(month);
+  console.log(recentMonthData);
+  const data = {
+    labels: monthList,
+    datasets: [
       {
-        ticks: {
-          beginAtZero: true,
-        },
+        label: '단위 : 억',
+        data: recentMonthData,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
       },
     ],
-  },
-};
+  };
+  
+  const options = {
+    legend: {
+      display:false
+    },
+  
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
 
-const LineChart = () => {
   return(
     <>
     <div className='header'>
-      <h1 className='title'>Line Chart</h1>
-      <div className='links'>
-        <a
-          className='btn btn-gh'
-          href='https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/Line.js'
-        >
-          Github Source
-        </a>
-      </div>
     </div>
     <Line data={data} options={options} />
   </>
   );
   }
 
-export default LineChart;
+export default DetailChart;
--- client/src/components/SampleData.js
+++ client/src/components/SampleData.js
@@ -4,91 +4,104 @@ export function SampleData(){
             address : "경기도 성남시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
-        }
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
+        },
         },
         {rank : 2, apt_name : "xx2아파트", apt_price : "20000원", details : {
             address : "경기도 시흥시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 3, apt_name : "xx3아파트", apt_price : "30000원", details : {
             address : "경기도 성남시 3번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 4, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 4번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 5, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 5번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 6, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 7, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 8, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 21번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 9, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 10, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 11, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 12, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
         {rank : 13, apt_name : "xx4아파트", apt_price : "40000원", details : {
             address : "경기도 성남시 1번지",
             area : 87.236,
             yearMonthDay : "2021-07-31",
-            floor : 15
+            floor : 15,
+            recentMonth : [11.2, 14.3, 21.1, 19.8, 20.3, 22.9]
         }
         },
     ];
